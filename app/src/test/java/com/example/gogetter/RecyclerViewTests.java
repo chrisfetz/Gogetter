995c6f2243e2d7dcfe19924a7b911624b6c91a9b
Chris Fetz <chrisrfetz@gmail.com>
2019-03-09 02:21:33 -0500

    Practice with RecyclerView sample by Suragch on StackOverflow

---
+++ app/src/test/java/com/example/gogetter/RecyclerViewTests.java
@@ -0,0 +1,9 @@
+package com.example.gogetter;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class RecyclerViewTests {
+
+}
