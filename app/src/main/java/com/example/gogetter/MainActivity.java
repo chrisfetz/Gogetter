8f3a0ef24514dd66bc35af797ed0321c0f1ab93a
Chris Fetz <chrisrfetz@gmail.com>
2019-03-09 20:00:02 -0500

    Added FAB

--- app/src/main/java/com/example/gogetter/MainActivity.java
+++ app/src/main/java/com/example/gogetter/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.gogetter;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,7 +13,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
-    MyRecyclerViewAdapter adapter;
+    private static final String LOG = MainActivity.class.getSimpleName();
+
+    private MyRecyclerViewAdapter mAdapter;
+    private FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +31,27 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         animalNames.add("Sheep");
         animalNames.add("Goat");
 
+        //setting onclick for Floating action button
+        mFab = findViewById(R.id.fab);
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                Toast.makeText(MainActivity.this, "Floating Action Button clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.rvAnimals);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyRecyclerViewAdapter(this, animalNames);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
+        mAdapter = new MyRecyclerViewAdapter(this, animalNames);
+        mAdapter.setClickListener(this);
+        recyclerView.setAdapter(mAdapter);
     }
 
     @Override
     public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "You clicked " + mAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
+
+
 }
